848eb96d429128ed91fed0fded40accec5f341a0
preetam mondal <preetam.mondal@cred.club>
2025-09-13 12:14:21 +0530

    Initial Commit

---
+++ rockPaperScissor/rockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockPaperScissorApp.swift
+//  rockPaperScissor
+//
+//  Created by Preetam Mondal on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct rockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rockPaperScissorTests/rockPaperScissorTests.swift
@@ -0,0 +1,17 @@
+//
+//  rockPaperScissorTests.swift
+//  rockPaperScissorTests
+//
+//  Created by Preetam Mondal on 13/09/25.
+//
+
+import Testing
+@testable import rockPaperScissor
+
+struct rockPaperScissorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
